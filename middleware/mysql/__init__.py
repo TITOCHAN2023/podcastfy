8fda91cee6d5063b32ea905ce8ed358b7cc47cdb
陳展鵬 <zzzzz@chintenhous-MacBook-Air.local>
2024-12-17 10:49:00 +0800

    fix2pgsql

--- middleware/mysql/__init__.py
+++ middleware/mysql/__init__.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 import os
+import json
+from urllib.parse import quote_plus
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -11,28 +13,33 @@ MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
 MYSQL_USER = os.getenv("MYSQL_USER")
-
+PGSQLDB_CONFIG= os.getenv("PGSQLDB_CONFIG")
 from logger import logger
 
 from .models import BaseSchema
 
 MYSQL_LINK = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
-
+pgsqldb_config =eval(PGSQLDB_CONFIG)
 
 @logger.catch
 def init_mysql_session() -> sessionmaker[Session]:
 
-    engine = create_engine(
-        MYSQL_LINK,
-        connect_args={
-            "charset": "utf8mb4",
-        },
-        pool_size=10,
-        max_overflow=20,
-        pool_pre_ping=True,
-        pool_use_lifo=True,
-        pool_recycle=3600,
-    )
+    # engine = create_engine(
+    #     MYSQL_LINK,
+    #     connect_args={
+    #         "charset": "utf8mb4",
+    #     },
+    #     pool_size=10,
+    #     max_overflow=20,
+    #     pool_pre_ping=True,
+    #     pool_use_lifo=True,
+    #     pool_recycle=3600,
+    # )
+
+
+    password = quote_plus(pgsqldb_config["password"])
+    engine = create_engine(f'postgresql+psycopg2://{pgsqldb_config["user"]}:{password}@{pgsqldb_config["host"]}:{pgsqldb_config["port"]}/{pgsqldb_config["dbname"]}',echo=True,client_encoding='utf8')
+
 
     session = sessionmaker(bind=engine)
 
